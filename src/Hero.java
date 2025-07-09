bd8799cc86598739501abe0bbebc7d1c67ed8797
241005 <241005@std.hi-joho.ac.jp>
2025-07-09 09:15:03 +0900

    エラー

--- src/Hero.java
+++ src/Hero.java
@@ -1,1 +1,8 @@
-public class Hero extends Character {}
+public class Hero extends Character {
+    @Override
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        System.out.println("敵に10のダメージを与えた");
+        m.hp -= 10;
+    }
+}
--- src/Wizard.java
+++ src/Wizard.java
@@ -1,1 +1,8 @@
-public class Wizard extends Character {}
+public class Wizard extends Character {
+    @Override
+    public void attack(Matango m) {
+        System.out.println("魔法使いは火の玉を放った！");
+        System.out.println("敵に7のダメージを与えた");
+        m.hp -= 7;
+    }
+}
